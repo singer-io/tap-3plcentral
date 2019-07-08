db8476455752bb47ea104e4854f97c5b6bede03a
jeffhuth-bytecode <jeff.huth@bytecode.io>
2019-07-08 09:55:33 -0700

    Update setup with dependencies

--- setup.py
+++ setup.py
@@ -8,7 +8,11 @@ setup(name='tap-tplcentral',
       author='jeff.huth@bytecode.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_tplcentral'],
-      install_requires=[],
+      install_requires=[
+          'backoff==1.3.2',
+          'requests==2.20.0',
+          'singer-python==5.6.1'
+      ],
       entry_points='''
           [console_scripts]
           tap-tplcentral=tap_tplcentral:main
