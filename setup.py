bd4aadcd9d3034efa324d8879c7db45f82a9f3b0
Dylan Sprayberry <dylan.sprayberry@qlik.com>
2026-05-14 14:11:52 +0000

    Add urllib3 and bump reqeusts and singer-python

--- setup.py
+++ setup.py
@@ -10,8 +10,9 @@ setup(name='tap-3plcentral',
       py_modules=['tap_3plcentral'],
       install_requires=[
           'backoff==1.10.0',
-          'requests==2.32.5',
-          'singer-python==5.14.3'
+          'requests==2.33.0',
+          'singer-python==5.18.0'
+          'urllib3==2.7.0'
       ],
       entry_points='''
           [console_scripts]
